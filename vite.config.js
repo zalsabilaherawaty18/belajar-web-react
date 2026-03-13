f2958c4b362b85ee2bae904226819dd787d15592
zalsabilaherawaty18 <zalsabilaherawatyy18@gmail.com>
2026-03-13 14:29:06 +0700

    123

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
 // import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(() => {
+  return {
+    plugins: [react(), tailwindcss()],
+  };
 });
